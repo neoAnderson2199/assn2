76ed2c46d0e993b17bef1334ed96e7251571cc7c
Nicholas Bunnell <nbunnel1@cs.binghamton.edu>
2018-10-20 13:33:23 -0400

    added #include macros into .h, main method into .c file

---
+++ file_recognizer.c
@@ -0,0 +1,12 @@
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+#include "file_recognizer.h"
+
+int main() {
+	return 0;
+}
